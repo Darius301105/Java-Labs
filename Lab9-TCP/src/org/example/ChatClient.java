4b3c9ec3c1efc8dc10e03342c6efbdd34a524668
Darius301105 <dariusandreieftim@gmail.com>
2026-05-20 16:03:19 +0300

    Lab9-TCP

--- Lab9-TCP/src/org/example/ChatClient.java
+++ Lab9-TCP/src/org/example/ChatClient.java
@@ -11,7 +11,7 @@ public class ChatClient {
     public static void main(String[] args) {
         String host = "localhost";
         int port = 12345;
-
+        ..
         try (
                 Socket socket = new Socket(host, port);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
